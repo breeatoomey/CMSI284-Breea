905a301b271121162e49c1dd666392daa0f4f84c
Kimberly K <kimberly.n.kubo@gmail.com>
2022-09-14 18:34:21 -0700

    started nato

---
+++ classwork/classwork03/natoalphabet.py
@@ -0,0 +1,15 @@
+def nato_alphabet():
+    letter_to_nato_alphabet = {'a':'alpha', 'b':'bravo', 'c':'charlie', 
+    'd':'delta', 'e':'echo', 'f':'foxtrot', 'g':'golf', 'h':'hotel', 
+    'i':'india', 'j':'juliett', 'k':'kilo', 'l':'lima', 'm':'mike',
+    'n':'november', 'o':'oscar', 'p':'papa', 'q':'quebec', 'r':'romeo',
+    's':'sierra', 't':'tango', 'u':'uniform', 'v':'victor', 'w':'whiskey',
+    'x':'xray', 'y':'yankee', 'z':'zulu'}
+
+    message_initial = input('Enter message: ')
+    # May need to replace all non a-z characters
+    message_initial = message_initial.replace(' ', '').lower()
+    for char in message_initial:
+        print(letter_to_nato_alphabet[char])
+
+nato_alphabet()
