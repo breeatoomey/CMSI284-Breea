1d8bff354ed2d16a815419eeed6826b8063ef847
Breea Toomey <breeatoomey@Breeas-MacBook-Pro.local>
2022-09-19 15:12:59 -0700

    fixed bug to filter out non-alph. chars

--- classwork/classwork03/natoalphabet.py
+++ classwork/classwork03/natoalphabet.py
@@ -1,6 +1,6 @@
 def nato_alphabet():
-    letter_to_nato_alphabet = {'a':'alpha', 'b':'bravo', 'c':'charlie', 
-    'd':'delta', 'e':'echo', 'f':'foxtrot', 'g':'golf', 'h':'hotel', 
+    letter_to_nato_alphabet = {'a':'alpha', 'b':'bravo', 'c':'charlie',
+    'd':'delta', 'e':'echo', 'f':'foxtrot', 'g':'golf', 'h':'hotel',
     'i':'india', 'j':'juliett', 'k':'kilo', 'l':'lima', 'm':'mike',
     'n':'november', 'o':'oscar', 'p':'papa', 'q':'quebec', 'r':'romeo',
     's':'sierra', 't':'tango', 'u':'uniform', 'v':'victor', 'w':'whiskey',
@@ -8,7 +8,8 @@ def nato_alphabet():
 
     message_initial = input('Enter message: ')
     # May need to replace all non a-z characters
-    message_initial = message_initial.replace(' ', '').lower()
+    #essage_initial = message_initial.replace(' ', '').lower()
+    message_initial = ''.join(char for char in message_initial if char.isalpha()).lower()
     for char in message_initial:
         print(letter_to_nato_alphabet[char])
 
