a6b5b32cf19b15180b03927edcd8a3d131393d72
Breea Toomey <breeatoomey@Breeas-MacBook-Pro.local>
2022-09-19 15:16:57 -0700

    fixed non-alph. edge case

--- classwork/classwork03/morsecode.py
+++ classwork/classwork03/morsecode.py
@@ -1,6 +1,6 @@
 def morse_code():
-    letter_to_morse_code = {'a':'.-', 'b':'-...', 'c':'-.-.', 
-    'd':'-..', 'e':'.', 'f':'..-.', 'g':'--.', 'h':'....', 
+    letter_to_morse_code = {'a':'.-', 'b':'-...', 'c':'-.-.',
+    'd':'-..', 'e':'.', 'f':'..-.', 'g':'--.', 'h':'....',
     'i':'..', 'j':'.---', 'k':'-.-', 'l':'.-..', 'm':'--',
     'n':'-.', 'o':'---', 'p':'.--.', 'q':'--.-', 'r':'.-.',
     's':'...', 't':'-', 'u':'..-', 'v':'...-', 'w':'.--',
@@ -8,7 +8,7 @@ def morse_code():
 
     message_initial = input('Enter message: ')
     # May need to replace all non a-z characters
-    message_initial = message_initial.replace(' ', '').lower()
+    message_initial = ''.join(char for char in message_initial if char.isalpha()).lower()
     for char in message_initial:
         print(letter_to_morse_code[char])
 
