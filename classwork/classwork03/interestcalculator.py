c0f1e5b51268ba594180cf22cc4978ff981df672
Breea Toomey <breeatoomey@Breeas-MacBook-Pro.local>
2022-09-19 16:55:51 -0700

    improved interest calculator

--- classwork/classwork03/interestcalculator.py
+++ classwork/classwork03/interestcalculator.py
@@ -5,12 +5,12 @@
 def interest_calculator():
     initial = eval(input('Initial balance: '))
     rate = eval(input('Annual interest rate in percent: ')) / 100.00
+    n = eval(input('Number of times interest applied per time period: '))
+    t = eval(input('Number of time periods elapsed: '))
 
-    print('After first month: $' + calculate_final(initial, rate, 1)
-      + '\nAfter second month: $' + calculate_final(initial, rate, 2)
-      + '\nAfter third month: $' + calculate_final(initial, rate, 3))
+    print('Your final amount: $' + calculate_final(initial, rate, n, t))
 
-def calculate_final(initial, rate, month):
-    return format(initial * (1 + rate/12) ** month, '.2f')
+def calculate_final(initial, rate, n, t):
+    return format(initial * ((1 + rate/n) ** (n*t)), '.2f')
 
 interest_calculator()
