8868298d5b12ce91baae488b512d30d14511ca51
kimberlykubo <kimberly.n.kubo@gmail.com>
2022-11-01 11:45:13 -0700

    finished dec2hex

--- homework/homework05/dec2hex.c
+++ homework/homework05/dec2hex.c
@@ -3,4 +3,25 @@
  * modification, you must also handle an optional command line 
  * argument which indicates the number of bits that the output 
  * hex value will represent, either 32 or 64.
+ * 
+ * look up printf format specifiers
+ * dec to hex has been done already
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char * argv[] ) {
+    int decimal = atoi( argv[1] );
+    int bits = atoi( argv[2] );
+
+    if( bits == 64 ) {
+        printf("%016x", decimal);
+    }
+    
+    else {
+        printf("%08x", decimal);
+    }
+
+    return 0;
+}
