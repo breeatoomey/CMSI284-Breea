0da6837de1fbcf775609e3e96aec40d9222ba61b
kimberlykubo <kimberly.n.kubo@gmail.com>
2022-11-01 11:15:52 -0700

    completed dec2bin

--- homework/homework05/dec2bin.c
+++ homework/homework05/dec2bin.c
@@ -1,3 +1,27 @@
 /**
  * Convert a base-10 number to its 32-bit binary value equivalent.
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main( int argc, char * argv[] ) {
+    int binary[32];
+    int decimal = atoi( argv[1] );
+
+    for( int i = 0; i < 32; i++ ) {
+        binary[i] = 0;
+        if( decimal%2 == 1 ) {
+            binary[i] = 1;
+        }
+        decimal /= 2;
+    }
+
+    for( int i = 31; i >= 0; i-- ) {
+        printf("%d", binary[i]);
+        fflush(stdout);
+    }
+
+    return 0;
+}
