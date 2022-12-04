db04e5496a9017099f9adf68c7dd877ceeab24e3
kimberlykubo <kimberly.n.kubo@gmail.com>
2022-12-03 18:17:21 -0800

    progress on makeNBO

--- homework/homework06/makeNBO.c
+++ homework/homework06/makeNBO.c
@@ -11,7 +11,7 @@
 int main( int argc, char * argv[] ) {
     int x = 1;
     int byte = atoi(argv[1]);
-    if(*(char*)&x != 1) {
+    if(*(char*)&x == 1) {
         printf("little-endian");
         // printf("%l", htonl(byte));
     }
