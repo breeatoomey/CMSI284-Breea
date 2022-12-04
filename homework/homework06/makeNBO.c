cc2ae3683117ad45dfc15ad2a8dbfe3c35c94c50
kimberlykubo <kimberly.n.kubo@gmail.com>
2022-12-03 18:16:39 -0800

    progress on makeNBO

--- homework/homework06/makeNBO.c
+++ homework/homework06/makeNBO.c
@@ -3,3 +3,21 @@
  * if necessary, to be in Network Byte Order. Create a test program to call 
  * your function and display the results on stdout.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+
+int main( int argc, char * argv[] ) {
+    int x = 1;
+    int byte = atoi(argv[1]);
+    if(*(char*)&x != 1) {
+        printf("little-endian");
+        // printf("%l", htonl(byte));
+    }
+    else {
+        printf("big-endian");
+        printf("%d", byte);
+    }
+    return 0;
+}
