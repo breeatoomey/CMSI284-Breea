61966c4a70187e604946dd6cbb34dcb23841b37c
kimberlykubo <kimberly.n.kubo@gmail.com>
2022-12-05 17:16:50 -0800

    makeNBO progress

--- homework/homework06/makeNBO.c
+++ homework/homework06/makeNBO.c
@@ -1,23 +1,33 @@
 /**
- * Reads a number from the command line arguments and swaps its byte order, 
- * if necessary, to be in Network Byte Order. Create a test program to call 
+ * Reads a number from the command line arguments and swaps its byte order,
+ * if necessary, to be in Network Byte Order. Create a test program to call
  * your function and display the results on stdout.
-*/
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <winsock2.h>
 
-int main( int argc, char * argv[] ) {
+uint32_t makeNBO (uint32_t number) {
     int x = 1;
-    int byte = atoi(argv[1]);
-    if(*(char*)&x == 1) {
-        printf("little-endian");
-        // printf("%l", htonl(byte));
+    if (*(char *)&x == 1) {
+        uint32_t nboNumber = htonl(number);
+        return nboNumber;
+    } else {
+        return number;
     }
-    else {
-        printf("big-endian");
-        printf("%d", byte);
+    
+}
+
+int main (int argc, char* argv[]) {
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        printf("Failed. Error Code : %d", WSAGetLastError());
+        return 1;
     }
+    uint32_t number = (uint32_t) argv[1];
+    printf("%ul", makeNBO(number));
+    WSACleanup();
     return 0;
 }
--- homework/homework06/paritygen.nasm
+++ homework/homework06/paritygen.nasm
@@ -1,3 +1,15 @@
 ; Write a nasm function called paritygen.nasm that will count the one bits 
 ; in a byte of data and return the proper value for the parity bit assuming 
 ; we are using odd parity.
+
+
+; rax 01100110
+; rbx 00000001
+; rax 00000000
+; cmp rax, 1
+; jz count
+; shr rax, 1      shift right
+; jmp loop
+
+; count: inc rcx
+;        loop
