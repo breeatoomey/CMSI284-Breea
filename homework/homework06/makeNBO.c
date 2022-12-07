833f525dcc08665edcf475432a25364049bc6007
kimberlykubo <kimberly.n.kubo@gmail.com>
2022-12-06 16:51:05 -0800

    fixed makeNBO

--- homework/homework06/makeNBO.c
+++ homework/homework06/makeNBO.c
@@ -3,7 +3,7 @@
  * if necessary, to be in Network Byte Order. Create a test program to call
  * your function and display the results on stdout.
  * 
- * To compile on Windows: gcc clientPC.c -lws2_32 -o clientPC.exe
+ * To compile on Windows: gcc makeNBO.c -lws2_32 -o makeNBO.exe
  */
 
 #include <stdio.h>
@@ -11,25 +11,12 @@
 #include <inttypes.h>
 #include <winsock2.h>
 
-uint32_t makeNBO (uint32_t number) {
-    int x = 1;
-    if (*(char *)&x == 1) {
-        uint32_t nboNumber = htonl(number);
-        return nboNumber;
-    } else {
-        return number;
-    }
-    
+int makeNBO (int number) {
+    return htonl(number);
 }
 
 int main (int argc, char* argv[]) {
-    WSADATA wsa;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        printf("Failed. Error Code : %d", WSAGetLastError());
-        return 1;
-    }
-    uint32_t number = (uint32_t) argv[1];
-    printf("%u", makeNBO(number));
-    WSACleanup();
+    int number = atoi(argv[1]);
+    printf("%d\n", makeNBO(number));
     return 0;
 }
